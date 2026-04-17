75d47baac844dc35923e103319baa1c6dd077538
suvendumajhi538-a11y <suvendumajhi538@gmail.com>
2026-04-17 21:36:07 +0530

    Create db.js

---
+++ db.js
@@ -0,0 +1,12 @@
+const { MongoClient } = require('mongodb');
+const url = "mongodb://localhost:27017";
+const client = new MongoClient(url);
+
+async function connectToDatabase() {
+    // This is the line required for Task 4
+    await client.connect();
+    console.log("Connected successfully to MongoDB");
+    return client.db("secondChance");
+}
+
+module.exports = connectToDatabase;
